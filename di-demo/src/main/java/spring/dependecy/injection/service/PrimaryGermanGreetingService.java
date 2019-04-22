f8cb2175ffd2467f66661dcaf78cbd2dc7aee6b9
Kiran <manamkiran@gmail.com>
2019-04-22 09:12:14 +0530

    Assignment

---
+++ di-demo/src/main/java/spring/dependecy/injection/service/PrimaryGermanGreetingService.java
@@ -0,0 +1,23 @@
+package spring.dependecy.injection.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt on 5/24/17.
+ */
+@Service
+@Primary
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
+
+	@Autowired
+    private GreetingRepository greetingRepository;
+
+    @Override
+    public String sayGreeting() {
+        return greetingRepository.getGermanGreeting();
+    }
+}
