45bae0c089c9759c0a709dafaa5387c4a7495736
Kiran <manamkiran@gmail.com>
2019-04-23 13:15:13 +0530

    Owner page and controller

---
+++ petclinic/petclinicweb/src/main/java/spring/pet/petclinic/controllers/OwnersController.java
@@ -0,0 +1,17 @@
+package spring.pet.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnersController {
+	
+	@GetMapping({"/",""})
+	public String listOwners() {
+		
+		return "owners/listOwners";
+	}
+
+}
