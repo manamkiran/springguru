9cc145d2c0338a07549282854b458eba76e7a407
Kiran <manamkiran@gmail.com>
2019-05-06 11:00:55 +0530

    Adding code to list Vets from Map

--- petclinic/petclinicweb/src/main/java/spring/pet/petclinic/controllers/VetsController.java
+++ petclinic/petclinicweb/src/main/java/spring/pet/petclinic/controllers/VetsController.java
@@ -1,13 +1,26 @@
 package spring.pet.petclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import spring.pet.petclinic.services.VetService;
 
 @Controller
 public class VetsController {
-	
-	@GetMapping({"vets"})
-	public String listVets() {
+
+	private final VetService vetService;
+
+	@Autowired
+	public VetsController(VetService vetService) {
+		this.vetService = vetService;
+	}
+
+	@GetMapping({ "vets" })
+	public String listVets(Model model) {
+
+		model.addAttribute("vets", vetService.findAll());
 		
 		return "vets/listVets";
 	}
