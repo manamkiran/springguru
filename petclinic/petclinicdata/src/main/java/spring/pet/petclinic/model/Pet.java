6fcfa6933e78ec3a89d8029ab65c1a809402c412
Kiran <manamkiran@gmail.com>
2019-06-06 13:36:56 +0530

    Adding JPA to pet and visit

--- petclinic/petclinicdata/src/main/java/spring/pet/petclinic/model/Pet.java
+++ petclinic/petclinicdata/src/main/java/spring/pet/petclinic/model/Pet.java
@@ -1,10 +1,14 @@
 package spring.pet.petclinic.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +26,9 @@ public class Pet extends BaseEntity {
 	@ManyToOne
 	@JoinColumn(name = "owner_id")
 	private Owner owner;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy="pet")
+	private Set<Visit> visits = new HashSet<Visit>();
 
 	public String getName() {
 		return name;
